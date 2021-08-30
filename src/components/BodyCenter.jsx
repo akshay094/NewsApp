c89b10cd1ff536d0b0028b110aa1165b41a343fd
Akshay kanojia <akanojia.47@gmail.com>
2021-08-30 17:39:36 +0530

    Api changed

--- src/components/BodyCenter.jsx
+++ src/components/BodyCenter.jsx
@@ -1,16 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Selector from './Selector'
-import { Grid, Typography, Box } from '@material-ui/core';
+import { Grid, Typography, Box, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
 import { Pagination } from '@material-ui/lab';
 import ImgMediaCard from './Card'
 
-
-const API_KEY = `${process.env.REACT_APP_API_KEY}`;
-
-const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,39 +22,51 @@ const useStyles = makeStyles((theme) => ({
 
 const BodyCenter = () => {
 
+  let date = new Date();
+  let dateStr = (date.getMonth() < 10) ? date.getFullYear() + '-0' + date.getMonth() + '-' + date.getDate() : date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate() + " " + "00:00:00";
+
+
   const [spacing, setSpacing] = React.useState(2);
   const classes = useStyles();
 
-  const handleChange = (event) => {
-    setSpacing(Number(event.target.value));
-  };
-
   let [topHeadData, setTopHeadData] = useState([]);
-
-  let [category, setCategory] = useState('top')
-  let [country, setCountry] = useState('in')
-  let [q, setQ] = useState('spacex')
+  let [from, setFrom] = useState(dateStr);
+  let [to, setTo] = useState(dateStr);
+  let [category, setCategory] = useState('news')
+  let [country, setCountry] = useState('IN')
+  // let [q, setQ] = useState('news')
   let [sortBy, setSortBy] = useState('popularity')
   let [page, setPage] = useState(1)
   let [totalPages, setTotalPages] = useState(10);
   const sortByArr = ['relevancy', 'popularity', 'publishedAt']
 
-  const countryArr = { India: 'in', USA: 'us', Canada: 'ca', Australia: 'au', Japan: 'jp' }
+  // const countryArr = { India: 'IN', USA: 'US', France: 'FR', 'United Kingdom': 'GB', Australia: 'AU', Japan: 'JP', Russia: 'RU' }
+  // date format = YYYY/MM/DD
+  // const headlineArr = ['top-headlines', 'everything']
 
-  const headlineArr = ['top-headlines', 'everything']
-
-  const categoryArr = ['business', 'entertainment', 'top', 'health', 'science', 'sports', 'technology']
-
-  let date = new Date();
-  let dateStr = date.getFullYear() + date.getMonth() + date.getDate();
-
+  const categoryArr = ['news', 'sport', 'tech', 'finance', 'politics', 'business', 'economics', 'entertainment', 'beauty', 'gaming']
 
   function handleClick(e) {
     setPage(e.target.innerText)
   }
 
-  useEffect(() => {
+  function handleToDate(e) {
+    let val = e.target.value;
+    let result = val.split('-').join('/') + " " + "00:00:00";
+    console.log(result);
+    setTo(result);
+  }
 
+  function handleFromDate(e) {
+    let val = e.target.value;
+    let result = val.split('-').join('/') + " " + "00:00:00";
+    console.log(result);
+    setFrom(result);
+  }
+
+  useEffect(() => {
+    console.log(to);
+    console.log(from);
     const ever = {
       method: 'GET',
       url: ``,
@@ -67,20 +74,24 @@ const BodyCenter = () => {
         accept: 'application/json',
       }
     };
-    // https://newsdata.io/api/1/sources?country=${country}&category=${category}&language=en&&page=${page}&apiKey=${API_KEY}
-    const topHead = {
+    // , from: `${from}`, to: `${to}`
+    var topHead = {
       method: 'GET',
-      url: `${proxyUrl}https://newsdata.io/api/1/news?country=${country}&category=${category}&language=en&page=${page}&apiKey=${API_KEY}`,
+      url: 'https://free-news.p.rapidapi.com/v1/search',
+      params: { q: `${category}`, lang: 'en', page: `${page}`, page_size: '10' },
       headers: {
-        accept: 'application/json',
+        'x-rapidapi-host': 'free-news.p.rapidapi.com',
+        'x-rapidapi-key': '1272c6971bmsh34e8d3c1bf22d59p14bc93jsn4b0e37fc06b2'
       }
     };
+
     const sendReq = async () => {
       try {
         let response = await axios.request(topHead);
-        console.log(response.data.results);
-        setTotalPages(Math.ceil((response.data.totalResults) / 10));
-        setTopHeadData(response.data.results)
+        console.log(response.data.articles);
+        // console.log(response.data.total_pages);
+        setTotalPages(Math.ceil((response.data.total_pages) / 10));
+        setTopHeadData(response.data.articles)
 
       } catch (err) {
         console.error(err);
@@ -92,12 +103,38 @@ const BodyCenter = () => {
 
   return (
     <>
+      <br />
+      <br />
       <Grid container justifyContent="center" className={classes.root} setSpacing={2} align="center">
+
         <Grid item xs={6}>
-          <Box variant="div">
-            <Typography variant="p">Country</Typography>
-          </Box>
-          <Selector name="Country" list={countryArr} selection={setCountry} value={country} />
+          <form className={classes.container} noValidate>
+            <TextField
+              id="date"
+              label="From"
+              type="date"
+              onChange={handleToDate}
+              defaultValue={dateStr}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </form>
+          <br />
+          <form className={classes.container} noValidate>
+            <TextField
+              id="date"
+              label="To"
+              type="date"
+              onChange={handleFromDate}
+              defaultValue={dateStr}
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </form>
         </Grid>
         <Grid item xs={6}>
           <Box variant="div">
@@ -106,7 +143,8 @@ const BodyCenter = () => {
           <Selector name="Category" list={categoryArr} selection={setCategory} value={category} />
         </Grid>
       </Grid>
-
+      <br />
+      <br />
       <Grid container justifyContent="center" className={classes.root} setSpacing={2} align="center">
 
         <Grid item xs={12}>
@@ -115,12 +153,11 @@ const BodyCenter = () => {
           </Box>
         </Grid>
         {
-          topHeadData.filter((data) => data.image_url
-          ).map((item, index) => {
+          topHeadData.map((item, index) => {
             return (
               <Grid item xs={12} key={index}>
                 <Box key={index} display="flex" alignItems="center" justifyContent="center" m={3} p={1}>
-                  <ImgMediaCard key={index} img={item.image_url} title={item.title} url={item.link} date={item.pubDate} description={item.content} />
+                  <ImgMediaCard key={index} img={item.media} title={item.title} url={item.link} date={item.published_date} description={item.summary} />
                 </Box>
               </Grid>
             )
